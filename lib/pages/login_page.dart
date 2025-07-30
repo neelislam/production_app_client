705eb3047cbf0c33f4c3a7839d34528133c0102c
Rabiul Islam Apu (Neel) <rabiul17.ri@gmail.com>
2025-07-30 23:05:38 +0600

    Secret

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,8 +6,36 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-
+      body: Container(
+        width: double.maxFinite,
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Colors.blueGrey[50],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Welcome Back!',
+            style: TextStyle(
+              fontSize: 38,
+              fontWeight: FontWeight.bold,
+              color: Colors.deepPurple,
+            ),
+            ),
+            SizedBox(height: 10,),
+            TextField(
+              keyboardType: TextInputType.phone,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                prefixIcon: Icon(Icons.phone_android),
+                labelText: 'Mobile Number',
+                hintText: 'Enter your mobile number',
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
