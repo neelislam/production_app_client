566be1ee6cbe43acf0e2cda6ed77eb3f288a3dfb
Rabiul Islam Apu (Neel) <rabiul17.ri@gmail.com>
2025-08-01 00:13:59 +0600

    3hr 46 min

---
+++ lib/firebase_option.dart
@@ -0,0 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
+
+FirebaseOptions firebaseOptions = FirebaseOptions(
+    apiKey: "AIzaSyAVz4vNoGOir9rKuy3Lmi5SC-6orl5Nxhw",
+    appId: "1:453457133601:android:4d5bfa1466baaf78eb2360",
+    messagingSenderId: "453457133601",
+    projectId: "production-app-project"
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:production_app_client/pages/home_page.dart';
 import 'package:production_app_client/pages/login_page.dart';
 import 'package:production_app_client/pages/register_page.dart';
 
-void main() {
+import 'firebase_option.dart';
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: firebaseOptions);
   runApp(const MyApp());
 }
 
