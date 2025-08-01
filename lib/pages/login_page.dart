aa0423051cd1daa362f996697c001977239c0b6d
Rabiul Islam Apu (Neel) <rabiul17.ri@gmail.com>
2025-08-01 22:36:01 +0600

    Fixed OTP field 4 42

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,54 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/simple/get_state.dart';
+import 'package:production_app_client/controller/login_controller.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: double.maxFinite,
-        padding: const EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          color: Colors.blueGrey[50],
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Welcome Back!',
-            style: TextStyle(
-              fontSize: 38,
-              fontWeight: FontWeight.bold,
-              color: Colors.deepPurple,
-            ),
-            ),
-            SizedBox(height: 10,),
-            TextField(
-              keyboardType: TextInputType.phone,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
+    return GetBuilder<LoginController>(builder: (logic) {
+      return Scaffold(
+        body: Container(
+          width: double.maxFinite,
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.blueGrey[50],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Welcome Back!',
+                style: TextStyle(
+                  fontSize: 38,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.deepPurple,
                 ),
-                prefixIcon: Icon(Icons.phone_android),
-                labelText: 'Mobile Number',
-                hintText: 'Enter your mobile number',
               ),
-            ),
-            SizedBox(height: 10,),
-            ElevatedButton(onPressed: (){},
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  backgroundColor: Colors.deepPurple,
+              SizedBox(height: 10,),
+              TextField(
+                keyboardType: TextInputType.phone,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  prefixIcon: Icon(Icons.phone_android),
+                  labelText: 'Mobile Number',
+                  hintText: 'Enter your mobile number',
                 ),
-                child: Text('Login')
-            ),
-            SizedBox(height: 10,),
-            TextButton(onPressed: (){},
-                child: Text('Register new account')
-            )
-          ],
+              ),
+              SizedBox(height: 10,),
+              ElevatedButton(onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.deepPurple,
+                  ),
+                  child: Text('Login')
+              ),
+              SizedBox(height: 10,),
+              TextButton(onPressed: () {},
+                  child: Text('Register new account')
+              )
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
