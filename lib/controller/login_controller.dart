3331fba11676c844a6a8e32413fb1126ac30d369
Rabiul Islam Apu (Neel) <rabiul17.ri@gmail.com>
2025-08-01 12:17:48 +0600

    Otp configured 4:26

--- lib/controller/login_controller.dart
+++ lib/controller/login_controller.dart
@@ -1,6 +1,9 @@
+import 'dart:math';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:otp_text_field_v2/otp_field_v2.dart';
 
 import '../models/user/user.dart';
 
@@ -11,6 +14,10 @@ class LoginController extends GetxController {
   TextEditingController registerNameController = TextEditingController();
   TextEditingController registerNumberController = TextEditingController();
 
+  OtpFieldControllerV2 otpController = OtpFieldControllerV2();
+  bool otpFieldShow = false;
+  int? otpSend ;
+  int? otpEntered;
 
 
   @override
@@ -22,29 +29,70 @@ class LoginController extends GetxController {
   adduser(){
 
     try {
+      if(otpSend == otpEntered){
+        DocumentReference doc = userCollection.doc();
+        User user = User(id: doc.id,
+            name: registerNameController.text,
+            number:  int.parse(registerNumberController.text));
+
+        final userJson = user.toJson();
+        doc.set(userJson);
+        Get.snackbar(
+          'Success',
+          'User added successfully',
+          colorText: Colors.green,
+        );
+
+      } else {
+        Get.snackbar(
+          'Error',
+          'OTP not correct!',
+          colorText: Colors.green,
+        );
+
+      }
+
+    } catch (e) {
+      Get.snackbar('Error', e.toString(), colorText: Colors.red);
+      print(e);
+    }
+  }
+
+
+
+  sendOtp(){
+    try {
       if(registerNameController.text.isEmpty || registerNumberController.text.isEmpty){
         Get.snackbar(
           'Error',
-          'Please enter yur name and number',
+          'Empty or Wrong input',
           colorText: Colors.red,
         );
         return;
       }
-      DocumentReference doc = userCollection.doc();
-      User user = User(id: doc.id,
-          name: registerNameController.text,
-          number:  int.parse(registerNumberController.text));
+      final random = Random();
+      int otp = 1000+ random.nextInt(9000);
+      print(otp);
+      if(otp != null){
+            otpFieldShow = true;
+            otpSend = otp;
+            Get.snackbar(
+              'Success',
+              'Otp sent Successfully!!',
+              colorText: Colors.blue,
+            );
 
-      final userJson = user.toJson();
-      doc.set(userJson);
-      Get.snackbar(
-        'Success',
-        'User added successfully',
-        colorText: Colors.green,
-      );
+          } else {
+            Get.snackbar(
+              'Error',
+              'Otp not send !!',
+              colorText: Colors.red,
+            );
+          }
     } catch (e) {
-      Get.snackbar('Error', e.toString(), colorText: Colors.red);
       print(e);
+    } finally {
+      update();
     }
   }
 }
